0f39e9230ccc338bcd44d4daf780e67419e47bee
John Codeos <johncodeos@gmail.com>
2022-06-14 19:55:36 +0300

    Update Gradle, Dependencies and Kotlin version

--- app/src/main/java/com/example/dependencieslicensesexample/MainActivity.kt
+++ app/src/main/java/com/example/dependencieslicensesexample/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.dependencieslicensesexample
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        license_btn.setOnClickListener {
+        val licenseButton = findViewById<Button>(R.id.license_btn)
+        licenseButton.setOnClickListener {
             OssLicensesMenuActivity.setActivityTitle("Third-Party Licenses")
             startActivity(Intent(this, OssLicensesMenuActivity::class.java))
         }
